a771211147bafc641da030d0dad4021783e5945c
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-03-17 11:09:00 +0100

    Missing constraint on slopecat. Area done

--- SkiAreaOpgave/Models/Area.cs
+++ SkiAreaOpgave/Models/Area.cs
@@ -8,5 +8,12 @@ namespace SkiAreaOpgave.Models
 {
     public class Area
     {
+        public string Name { get; set; }
+
+        public List<Slope> Slopes { get; set; }
+        public List<Skilift> Skilifts { get; set; }
+        public List<Skipass> Skipasses { get; set; }
+
+
     }
 }
--- SkiAreaOpgave/Models/Slope.cs
+++ SkiAreaOpgave/Models/Slope.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace SkiAreaOpgave.Models
     public class Slope
     {
         public string Name { get; set; }
-        public string SlopeCat { get; set; }
+        public string SlopeCat { get; set; } 
         public bool Primed { get; set; }
 
         public List<Area> Areas { get; set; }
