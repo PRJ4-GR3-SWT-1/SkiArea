9e9b6474a61231ce1ea98579bc516a37556ea0f9
Laurits H.G <201905745@post.au.dk>
2021-03-17 10:58:43 +0100

    Added Skilift

---
+++ SkiAreaOpgave/Models/Skilift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiAreaOpgave.Models
+{
+    class Skilift
+    {
+        private int id;
+        public string to;
+        public string from;
+        public float speed;
+        public int capacity;
+        public Area area;
+    }
+}
